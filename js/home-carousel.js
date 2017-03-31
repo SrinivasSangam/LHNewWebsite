0fc778c3a513c461dbed84b3b0ca129d1a832318
SrinivasSangam <ssangam@liquidhub.com>
2017-03-31 19:07:19 +0530

    31Mar2017-V1

--- js/home-carousel.js
+++ js/home-carousel.js
@@ -33,24 +33,30 @@ setTimeout(function(){$('#dvLoading').fadeOut();}, 2000);
         var carouselIndicatorsOffset = carouselIndicators.offset();   
         
           if (ratio_cont > ratio_img) {
+            
             $(".carousel-inner").css({"width": "100%", "height": $img.height()+"px"});
+            $(".carousel-inner .item").css({"width": "100%", "height": $img.height()+"px"});
             $(".carousel-caption").css({"margin-top": $img.height()/100*3.3333+"%"});
             $(".carousel-indicators").css({"top": $img.height()/100*88+"px"});                
           }
           else if (ratio_cont < ratio_img) {
 
             $(".carousel-inner").css({"width": "auto", "height": $img.height()+"px"});
+            $(".carousel-inner .item").css({"width": "auto", "height": $img.height()+"px"});
             $(".carousel-caption").css({"margin-top": $img.height()/100*5.3333+"%"});
+
           }
           if (ratio_img <= 1024 || ratio_img < 980) {
             $(".carousel-caption").css({"margin-top": $img.height()/100*3.5+"%"});
             $(".carousel-indicators").css({"top": $img.height()/100*85+"px"});
-          }  else 
-          if ($(this).width() < 768 ) {
+          }   
+          if ($(this).width() <= 768 ) {
             $(".carousel-caption").css({"margin-top": 18+"%"});
+            $(".carousel-indicators").css({"top": 250+"px"});  
           }
           if ($(this).width() < 737 ) {
             $(".carousel-inner").css({"width": "auto", "height": 350+"px","margin-bottom": 30+"px"});
+            $(".carousel-inner .item").css({"width": "auto", "height": 350+"px","margin-bottom": 30+"px"});
             $(".carousel-inner > .item > img").css({"display": "none"});
             $(".carousel-inner > .first-item").css({"background": "url('img/homepage-img1.jpg') center center","background-size": "cover"});
             $(".carousel-inner > .second-item").css({"background": "url('img/homepage-img2.jpg') center center","background-size": "cover"});
